78d32eadbaf4d9fbde706e128dbfe38fcb61b9f5
lupheron <nexcode17@gmail.com>
2026-03-04 22:16:40 +0500

    main page

--- src/components/Container/MainContainer.tsx
+++ src/components/Container/MainContainer.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Header from '../Header/Header'
-
+import Home from '../HomePage/Home'
 const MainContainer = () => {
     return (
         <div className='bg-primary font-roboto px-[40px] py-5' >
             <Header />
+            <div className='mt-30'>
+                <Home />
+            </div>
         </div>
     )
 }
