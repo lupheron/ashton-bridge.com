ccc9badc6d98c5118e2384d54886a36290e302a5
lupheron <nexcode17@gmail.com>
2026-03-04 21:28:10 +0500

    fonts, header, global css

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import MainContainer from "../components/Container/MainContainer";
+
+export default function Home() {
+  return (
+    <div className="">
+      <MainContainer />
+    </div>
+  );
+}
---
+++ src/components/Container/MainContainer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../Header/Header'
+
+const MainContainer = () => {
+    return (
+        <div className='bg-primary font-roboto px-[40px] py-5' >
+            <Header />
+        </div>
+    )
+}
+
+export default MainContainer
