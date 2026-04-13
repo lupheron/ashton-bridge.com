828c0d6bee996920530a270661deee3052469297
lupheron <nexcode17@gmail.com>
2026-04-13 21:55:27 +0500

    faq

--- src/components/Faq/Faq.tsx
+++ src/components/Faq/Faq.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useState } from 'react'
+import { useState } from 'react'
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown'
 
 const faqItems = [
